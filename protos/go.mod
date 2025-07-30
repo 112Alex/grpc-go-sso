b0ad6703f7d6d0ed3a21622a19c6253763efe5bb
sasha <sasasuhinin170@gmail.com>
2025-07-30 17:07:10 +0300

    Initial commit with SSO and protos

---
+++ protos/go.mod
@@ -0,0 +1,3 @@
+module github.com/112Alex/grpc-go-sso/protos
+
+go 1.24.0
---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,10 @@
+package sso
+
+func main() {
+	// TODO: initialize config object
+
+	// TODO: initialize logger (slog)
+
+	// TODO: initialize app
+	// NOTE: Само приложение, а не точка входа
+}
